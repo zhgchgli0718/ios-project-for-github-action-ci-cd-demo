2aa2f82e7624930eab2f1ebccc6d450764973070
ZhgChgLiBot <no-reply@zhgchg.li>
2025-01-12 19:32:11 +0800

    update cd fastlane

--- fastlane/README.md
+++ fastlane/README.md
@@ -23,6 +23,22 @@ For _fastlane_ installation instructions, see [Installing _fastlane_](https://do
 
 Run tests for iOS Project
 
+### ios archive
+
+```sh
+[bundle exec] fastlane ios archive
+```
+
+
+
+### ios deploy_to_firebase
+
+```sh
+[bundle exec] fastlane ios deploy_to_firebase
+```
+
+
+
 ----
 
 This README.md is auto-generated and will be re-generated every time [_fastlane_](https://fastlane.tools) is run.
--- iOS Project/ViewController.swift
+++ iOS Project/ViewController.swift
@@ -6,12 +6,22 @@
 //
 
 import UIKit
+import WebKit
+import ZMarkupParser
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var footerLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        webView.load(URLRequest(url: URL(string: "https://zhgchg.li")!))
+        
+        let parser = ZHTMLParserBuilder.initWithDefault().build()
+        footerLabel.attributedText = parser.render(#"© <strong>2025</strong>  <a href="https://zhgchg.li" target="_blank">ZhgChgLi</a>."#)
     }
 
 
